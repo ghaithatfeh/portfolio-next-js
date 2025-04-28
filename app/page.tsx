eb5fbf9b255cc86fc2049bf17b59f67a8490b274
Ghaith Atfeh <85496434+ghaithatfeh@users.noreply.github.com>
2025-04-28 23:49:43 +0300

    Dynamic import for client components

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
 import "aos/dist/aos.css";
-import Header from "./component/Header";
 import Hero from "./component/Hero";
 import Features from "./component/Features";
 import Services from "./component/Services";
 import About from "./component/About";
 import Contact from "./component/Contact";
 import Footer from "./component/Footer";
-import PageLoading from "./component/PageLoading";
-import Portfolio from "./component/Portfolio";
-import MyClientComponent from "./component/MyClientComponent";
-import MyServerComponent from "./component/MyServerComponent";
+// import Header from "./component/Header";
+// import PageLoading from "./component/PageLoading";
+// import Portfolio from "./component/Portfolio";
+import dynamic from "next/dynamic";
+
+const PageLoading = dynamic(() => import("./component/PageLoading"), { ssr: false });
+const Header = dynamic(() => import("./component/Header"), { ssr: false });
+const Portfolio = dynamic(() => import("./component/Portfolio"), { ssr: false });
 
 export default function Home() {
 	return (
